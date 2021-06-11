2a50cbcfad9e566d5c2032a4ba1f575777be5168
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-06-10 22:22:41 -0300

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_gray_Uri) VALUES ('Bootcamp HTML','https://cdn.pixabay.com/photo/2020/05/31/09/40/online-course-5242018_960_720.jpg','https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
+
+INSERT INTO tb_offer (edition,start_moment,end_moment,course_id) VALUES ('1.0','2020-11-20T03:00:00Z','2021-11-20T03:00:00Z',1);
+INSERT INTO tb_offer (edition,start_moment,end_moment,course_id) VALUES ('2.0','2020-12-20T03:00:00Z','2021-12-20T03:00:00Z',1);
